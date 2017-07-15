96a83b5c28ab90bcb42338a9738b0b70591a8376
Daniel Yim <danielyim@gmail.com>
2017-07-15 05:10:36 -0700

    get location retrieval working

--- App.js
+++ App.js
@@ -1,14 +1,51 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button, Divider } from 'react-native'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      message: ''
+    }
+  }
+
+  getLoc = () => {
+    navigator.geolocation.getCurrentPosition(
+      pos => {
+        const crd = pos.coords
+
+        // console.log('Your current position is:')
+        // console.log(`Latitude : ${crd.latitude}`)
+        // console.log(`Longitude: ${crd.longitude}`)
+        // console.log(`More or less ${crd.accuracy} meters.`)
+        this.setState({
+          message: `lat: ${crd.latitude}, lon: ${crd.longitude}, accuracy: ${crd.accuracy}m`
+        })
+      },
+      () => {
+        this.setState({ message: 'error' })
+      },
+      {}
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.header}>loc</Text>
-        <Text>
-          Welcome to <strong>loc</strong>.
-        </Text>
+        <Text>Welcome to loc.</Text>
+        <Text />
+        <Text>Your current position is...</Text>
+        <View>
+          <Text />
+          <Text>
+            {!!this.state.message &&
+              <Text>
+                {this.state.message}
+              </Text>}
+          </Text>
+          <Button title="Get Location" onPress={this.getLoc} />
+        </View>
       </View>
     )
   }
